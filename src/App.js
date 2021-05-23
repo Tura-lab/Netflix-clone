2cc3fb129fe6a0485184f34b6cc0ae6460ed9da6
Tura Lab <tiruzertsedeke25@gmail.com>
2021-05-23 10:07:28 -0700

    starting to create the banner and the nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import requests from './requests';
 import './App.css';
 import Row from './Row';
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellloo</h1>
-      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals}/>
+      <Banner />
+      <Row 
+        title='Netflix Originals' 
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
       <Row title='Top Rated ' fetchUrl={requests.fetchTopRated}/>
       <Row title='Action Movies' fetchUrl={requests.fetchActionMovies}/>
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css'
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -18,15 +18,15 @@ function Row({title, fetchUrl}) {
     }, [fetchUrl]);
 
     return (
-        <div>
+        <div className='row'>
             <h2>{title}</h2>
             <div className='row__posters'>
                 {movies.map(movie => {
                     return(
                     <img
-                        className='row__poster'
+                        className={`row__poster ${isLargeRow ? "row__posterLarge" : ''}`}
                         key={movie.id} 
-                        src={`${base_url}${movie.poster_path}`} 
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name}/>
                     )
                 })}
